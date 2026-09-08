deb0642f4744eca65920c8e0f98249063655e4d2
Arish <arishwanthreddy@gmail.com>
2026-09-07 20:49:44 -0500

    Add size limit to express.urlencoded()

--- backend/src/app.js
+++ backend/src/app.js
@@ -49,7 +49,7 @@ app.use(cors({
 }));
 
 app.use(express.json({ limit: '50kb' }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true, limit: '50kb' }));
 app.use(cookieParser());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
